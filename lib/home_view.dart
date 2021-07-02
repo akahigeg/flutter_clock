ac85fdf52580844dc9a590fb42a51e011a727991
akahige <akahigeg@gmail.com>
2021-07-02 12:22:21 +0900

    ClockEditorクラスを別ファイルに分離

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:audioplayers/audioplayers.dart';
 
+import './clock_editor.dart';
+
 class Clock extends StatefulWidget {
   Clock({Key? key, required this.title}) : super(key: key);
 
@@ -23,40 +25,6 @@ class ClockTip extends StatelessWidget {
     return Text(
       'Tip Stateless',
     );
-  }
-}
-
-class ClockEditor {
-  static String changeMin(String min, String upOrDown) {
-    int newMin;
-    if (upOrDown == 'up') {
-      newMin = int.parse(min) + 1;
-    } else {
-      newMin = int.parse(min) - 1;
-    }
-    if (newMin == 100) {
-      newMin = 0;
-    }
-    if (newMin == -1) {
-      newMin = 99;
-    }
-    return newMin.toString().padLeft(2, '0');
-  }
-
-  static String changeSec(String sec, String upOrDown) {
-    int newSec;
-    if (upOrDown == 'up') {
-      newSec = int.parse(sec) + 1;
-    } else {
-      newSec = int.parse(sec) - 1;
-    }
-    if (newSec == 60) {
-      newSec = 0;
-    }
-    if (newSec == -1) {
-      newSec = 59;
-    }
-    return newSec.toString().padLeft(2, '0');
   }
 }
 
