49253c745fda0c42763b6e037fe160c25ea8d2ab
akahige <akahigeg@gmail.com>
2021-07-10 21:54:09 +0900

    homeに指定しているFlutterTimerからTimerWidgetを分離

--- lib/timer_view.dart
+++ lib/timer_view.dart
@@ -13,23 +13,30 @@ class FlutterTimer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _pageController = PageController();
+
+    final List<Widget> _pages = <Widget>[];
+
+    return Scaffold(appBar: AppBar(title: Text("Flutter Clock"), actions: <Widget>[]), floatingActionButton: EditButton(), body: TimerWidget());
+  }
+}
+
+class TimerWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     Provider.of<TimerModel>(context, listen: false).restore();
 
-    return Scaffold(
-      appBar: AppBar(title: Text("Flutter Clock"), actions: <Widget>[]),
-      floatingActionButton: EditButton(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Consumer<TimerModel>(builder: (context, timer, child) {
-              return Column(children: [
-                timer.inEdit ? DisplayEdit() : Display(),
-                timer.inEdit ? InEditButtons() : ControlButtons(),
-              ]);
-            })
-          ],
-        ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Consumer<TimerModel>(builder: (context, timer, child) {
+            return Column(children: [
+              timer.inEdit ? DisplayEdit() : Display(),
+              timer.inEdit ? InEditButtons() : ControlButtons(),
+            ]);
+          })
+        ],
       ),
     );
   }
