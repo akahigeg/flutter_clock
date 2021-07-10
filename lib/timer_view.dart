c21817c513a5b46bb170a5812b850817f60812ec
akahige <akahigeg@gmail.com>
2021-07-10 22:04:19 +0900

    複数のTimerWidgetをPageContollerで制御してフリックで切り替えられるように

--- lib/timer_view.dart
+++ lib/timer_view.dart
@@ -15,13 +15,32 @@ class FlutterTimer extends StatelessWidget {
   Widget build(BuildContext context) {
     final _pageController = PageController();
 
-    final List<Widget> _pages = <Widget>[];
+    final List<Widget> _pages = <Widget>[
+      new TimerWidget("timer1"),
+      new TimerWidget("timer2"),
+    ];
 
-    return Scaffold(appBar: AppBar(title: Text("Flutter Clock"), actions: <Widget>[]), floatingActionButton: EditButton(), body: TimerWidget());
+    return Scaffold(
+        appBar: AppBar(title: Text("Flutter Clock"), actions: <Widget>[]),
+        floatingActionButton: EditButton(),
+        body: new IconTheme(
+            data: new IconThemeData(color: Colors.black.withOpacity(0.8)),
+            child: new Stack(children: <Widget>[
+              new PageView.builder(
+                  physics: new AlwaysScrollableScrollPhysics(),
+                  controller: _pageController,
+                  itemBuilder: (BuildContext context, int index) {
+                    return _pages[index % _pages.length];
+                  })
+            ])));
   }
 }
 
 class TimerWidget extends StatelessWidget {
+  TimerWidget(String timerId) {
+    final _timerId = timerId;
+  }
+
   @override
   Widget build(BuildContext context) {
     Provider.of<TimerModel>(context, listen: false).restore();
